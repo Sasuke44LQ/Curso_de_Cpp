62fbe18aa5f442ac22528074bd7f788358540471
Sasuke44LQ <sasuke44lq@gmail.com>
2025-11-16 10:10:29 -0500

    Guardando cambios V1.0

---
+++ C++/10_Funciones_Ejercicio_5.cpp
@@ -0,0 +1,18 @@
+//10. Funciones 
+//Ejercicio 5
+/* Escriba una funcion que use como base el contenido dado previamente en el curso y realice funciones para resolver todos los ejercicios */
+
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main(){
+    int x, y;
+
+    
+
+
+    getch();
+    return 0;
+}
